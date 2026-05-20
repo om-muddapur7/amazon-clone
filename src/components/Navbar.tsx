d1bb33e39c7a123931e36f5e844916dddcfe45f3
Om Muddapur <ommuddapur7@gmail.com>
2026-05-20 18:53:34 +0530

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./navbar.css"
+
+const Navbar = () => {
+	return (
+		<div>
+			<div className="nav">
+				<h1 className="title">amazon.in</h1>
+				<p className="para">Hello, User</p>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
