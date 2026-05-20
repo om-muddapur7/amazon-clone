d463dba53ef46a73395ce0c9b8108cc37340cc97
Om Muddapur <ommuddapur7@gmail.com>
2026-05-20 19:41:00 +0530

    wishlist page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar"
+import { Wishlist } from "./pages/Wishlist"
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <Wishlist />
     </>
   )
 }
---
+++ src/pages/Wishlist.tsx
@@ -0,0 +1,73 @@
+import Navbar from "../components/Navbar"
+import "./wishlist.css"
+
+export const Wishlist = () => {
+  return (
+    <div>
+        <Navbar />
+
+        <div className="container">
+            <div className="left">
+                <h2 id="l1">Your Wish List</h2>
+                <p id="l2">Default list</p>
+            </div>
+
+            <div className="right">
+                <div className="head-sec">
+                    <div className="hds1">
+                        <h2 id="r1">Your Wish List</h2>
+                        <p>Public</p>
+                    </div>
+
+                    <div className="hds2">
+                        <a href="#">Send list to others </a>
+
+                        <input type="text" placeholder="Search..." />
+
+                        <button>Filter & Sort</button>
+                    </div>
+                    
+                </div>
+
+                <div className="rgt-body">
+                    <div className="item">
+                        <img src="src\assets\hero.png" alt="" />
+
+                        <h2>Keyboard</h2>
+                        <h2>$2290.00</h2>
+
+                        <button className="btn2">Add to Cart</button>
+                    </div>
+
+                    <div className="item">
+                        <img src="src\assets\hero.png" alt="" />
+
+                        <h2>USB-C Adapter</h2>
+                        <h2>$289.00</h2>
+
+                        <button className="btn2">Add to Cart</button>
+                    </div>
+
+                    <div className="item">
+                        <img src="src\assets\hero.png" alt="" />
+
+                        <h2>Computer Monitor</h2>
+                        <h2>$999.00</h2>
+
+                        <button className="btn2">Add to Cart</button>
+                    </div>
+
+                    <div className="item">
+                        <img src="src\assets\hero.png" alt="" />
+
+                        <h2>Mouse</h2>
+                        <h2>$90.00</h2>
+
+                        <button className="btn2">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
